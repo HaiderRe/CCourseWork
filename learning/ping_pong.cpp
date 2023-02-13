10c094e93990f1cad7f981d5b9df9374cc57019b
HaiderRe <haider.rehawi1@gmail.com>
2023-02-13 22:52:46 +0000

    Start of creating ping pong
    Test project of ping pong so to learn the basics of raylib. Currently setup struct for the ball, and two paddles and have drawn them to the screen.

--- learning/ping_pong.cpp
+++ learning/ping_pong.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <cmath>
 #include "include/raylib.h"
+#include "include/rlgl.h"
+#include "include/raymath.h"
+
 /*******************************************************************************************
 *
 *   raylib [core] example - Basic window
@@ -30,14 +33,25 @@
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
+struct {             // Structure declaration
+  int x = 0;
+  int y = 0;         // Member (int variable)   // Member (string variable)
+} leftRect,rightRect, ball;
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
+    leftRect.x = 20;
+    leftRect.y = 540;
+    rightRect.x = 1900;
+    rightRect.y = 540;
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
+    unsigned int counter_of_loops = 0;
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    ball.x = static_cast<int>(GetScreenWidth() / 2); 
+    ball.y = static_cast<int>(GetScreenHeight() / 2 );
+
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -45,20 +59,30 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        
+            
+        
         // Update
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
+        if (IsKeyPressed(KEY_ENTER) && (IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT)))
+ 		{
+            ToggleFullscreen();
+        }
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            ClearBackground(BLACK);
+            DrawCircle(ball.x, ball.y, 10, RAYWHITE);
+            DrawRectangle(leftRect.x, leftRect.y, 4, 60, WHITE);
+            DrawRectangle(rightRect.x, rightRect.y, 4, 60, WHITE);
+            DrawFPS(10, 10);
 
         EndDrawing();
+        counter_of_loops++;
         //----------------------------------------------------------------------------------
     }
 
--- learning/ping_pong.ps1
+++ learning/ping_pong.ps1
@@ -1,3 +1,3 @@
-g++ ping_pong.cpp -o ping_pong.exe -O1 -Wall -std=c++11 -Wno-missing-braces -I include/ -L lib/ -lraylib -lopengl32 
+g++ ping_pong.cpp -o ping_pong.exe -O1 -Wall -std=c++11 -Wno-missing-braces -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
 start-sleep 1.2
 start-process ping_pong.exe
