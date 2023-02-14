bde7973384d2acd46ea226bf5fc2b8f64ef2e032
HaiderRe <haider.rehawi1@gmail.com>
2023-02-14 14:12:20 +0000

    Finished Ping Pong Game
    Finished ping pong game, has two players, moving, collision with the ball, speeding up every rally. Restarting after each goal. Score counter.

--- learning/ping_pong.cpp
+++ learning/ping_pong.cpp
@@ -33,9 +33,12 @@
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-struct {             // Structure declaration
+void should_move();
+void ball_speed(int &ballspeedx, int &ballspeedy);
+bool check_scored(unsigned int &l_score, unsigned int &r_score);
+struct objects{             // Structure declaration
   int x = 0;
-  int y = 0;         // Member (int variable)   // Member (string variable)
+  int y = 0;      // Member (int variable)   // Member (string variable)
 } leftRect,rightRect, ball;
 int main(void)
 {
@@ -45,10 +48,27 @@ int main(void)
     leftRect.y = 540;
     rightRect.x = 1900;
     rightRect.y = 540;
+    unsigned int left_rect_score = 0;
+    unsigned int right_rect_score = 0;
+    bool score_timer = false;
+    unsigned int counter_of_time = 1;
+    int ball_speedx = GetRandomValue(5, 8);
+    int ball_speedy = GetRandomValue(4, 7);
+    int tmp = GetRandomValue(0,3);
+    if(tmp == 0){
+        ball_speedx *= -1;
+    }
+    else if(tmp == 1){
+        ball_speedx *= -1;
+        ball_speedy *= -1;
+    }
+    else if(tmp == 3){
+        ball_speedy *= -1;
+    }
     const int screenWidth = 1920;
     const int screenHeight = 1080;
     unsigned int counter_of_loops = 0;
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Pong");
     ball.x = static_cast<int>(GetScreenWidth() / 2); 
     ball.y = static_cast<int>(GetScreenHeight() / 2 );
 
@@ -66,11 +86,42 @@ int main(void)
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
+        should_move();
+        if(!score_timer){
+        ball_speed(ball_speedx, ball_speedy);
+        }
+        else{
+            counter_of_time = counter_of_time + 1;
+            if(counter_of_time % 180 == 0){
+                counter_of_time = 1;
+                ball_speedx = GetRandomValue(5, 8);
+                ball_speedy = GetRandomValue(4, 7);
+                tmp = GetRandomValue(0,3);
+                if(tmp == 0){
+                    ball_speedx *= -1;
+                }
+                else if(tmp == 1){
+                    ball_speedx *= -1;
+                    ball_speedy *= -1;
+                }
+                else if(tmp == 3){
+                    ball_speedy *= -1;
+                }
+                
+                score_timer = false;
+            }
+        }
         if (IsKeyPressed(KEY_ENTER) && (IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT)))
  		{
             ToggleFullscreen();
         }
-
+        if(counter_of_loops % 30 == 0){
+            ball_speedx *= 1.04;
+            ball_speedy *= 1.02;
+        }
+       if(!score_timer){
+       score_timer = check_scored(left_rect_score, right_rect_score);
+       }
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
@@ -79,6 +130,9 @@ int main(void)
             DrawCircle(ball.x, ball.y, 10, RAYWHITE);
             DrawRectangle(leftRect.x, leftRect.y, 4, 60, WHITE);
             DrawRectangle(rightRect.x, rightRect.y, 4, 60, WHITE);
+            DrawRectangle(960,0, 4, 1080, WHITE);
+            DrawText(TextFormat(" %i", left_rect_score),490, 108, 32, WHITE);
+            DrawText(TextFormat(" %i", right_rect_score),1459, 108, 32, WHITE);
             DrawFPS(10, 10);
 
         EndDrawing();
@@ -92,4 +146,70 @@ int main(void)
     //--------------------------------------------------------------------------------------
 
     return 0;
+}
+void should_move(){
+    if(IsKeyDown(KEY_W)){
+        leftRect.y = leftRect.y - 10;
+    }
+    else if(IsKeyDown(KEY_S)){
+        leftRect.y = leftRect.y + 10;
+    }
+    if(IsKeyDown(KEY_DOWN)){
+        rightRect.y = rightRect.y + 10;
+    }
+    else if(IsKeyDown(KEY_UP)){
+        rightRect.y = rightRect.y - 10;
+    }
+    if(leftRect.y < 0){
+        leftRect.y = 0;
+    }
+    else if(leftRect.y > (GetScreenHeight() - 60)){
+        leftRect.y = GetScreenHeight() - 60;
+    }
+    if(rightRect.y < 0){
+        rightRect.y = 0;
+    }
+    else if(rightRect.y > GetScreenHeight() - 60){
+        rightRect.y = GetScreenHeight() - 60;
+    }
+}
+void ball_speed(int &ball_speedx, int &ball_speedy){
+    ball.y = ball.y + ball_speedy;
+    if(ball.y < 0 || ball.y > GetScreenHeight()){
+        ball_speedy *= -1;
+    }
+    ball.x = ball.x + ball_speedx; 
+    if(CheckCollisionCircleRec(Vector2{float(ball.x), float(ball.y)}, 10 , Rectangle{float(leftRect.x),float(leftRect.y), 4, 60 })){
+        ball_speedx *= -1;
+        if(ball.y > leftRect.y + 5){
+            ball_speedy = abs(ball_speedy) * -1;
+        }
+        else{
+            ball_speedy = abs(ball_speedy);
+        }
+    }
+    else if(CheckCollisionCircleRec(Vector2{float(ball.x), float(ball.y)}, 10 , Rectangle{float(rightRect.x),float(rightRect.y), 4, 60 })){
+        ball_speedx *= -1;
+        if(ball.y > rightRect.y + 5){
+            ball_speedy = abs(ball_speedy) * -1;
+        }
+        else{
+            ball_speedy = abs(ball_speedy);
+        }
+    }
+}
+bool check_scored(unsigned int &l_score, unsigned int &r_score){
+    if(ball.x < 0){
+        l_score = l_score + 1;
+        ball.x = 964;
+        ball.y = 540;
+        return true;
+    }
+    else if(ball.x > 1920){
+        r_score = r_score + 1;
+        ball.x = 964;
+        ball.y = 540;
+        return true;
+    }
+    return false;
 }
