bbff77f6ccbd602b3702ffa78552100b47a176bd
HaiderRe <haider.rehawi1@gmail.com>
2023-02-27 13:15:01 +0000

    Node class
    Created node class

--- learning/include/xml_parser.h
+++ learning/include/xml_parser.h
@@ -2,12 +2,27 @@
 #include <string> 
 #include <vector>
 #include <fstream>
+class tile_map_nodes{
+    tile_map_nodes *parent_node;
+    int width;
+    int height;
+};
 class file_to_read{
-    std::string path;
     int width; 
     int height; 
     bool read;
+    int nextlayerid;
+    int pixel_width;
+    int pixel_height;
     std::string *tilesets;
     public: 
-     void read_file();
+     void read_file(std::string path);
 };
+void read_file(std::string path){
+    std::ifstream tilemap; 
+    tilemap.open(path);
+    if(tilemap){
+
+    }
+
+}
