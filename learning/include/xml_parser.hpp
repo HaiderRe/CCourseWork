06930351de27164c617c0828d0f28d64b318508e
HaiderRe <haider.rehawi1@gmail.com>
2023-03-06 12:32:25 +0000

    Added Rapid xml instead of making own XML parser

---
+++ learning/include/xml_parser.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <fstream>
+#include "include/rapidxml.hpp"
+#include "include/rapidxml_iterators.hpp"
+#include "include/rapidxml_print.hpp"
+#include "include/rapidxml_utils.hpp"
+class file_to_read{
+    
+};
