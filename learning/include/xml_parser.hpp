8f26c924e23607fe6031d5e5eb3548feba4cf245
HaiderRe <haider.rehawi1@gmail.com>
2023-03-07 22:22:15 +0000

    Created Log Folder and Assets folder
    Created Log folder to store where I went wrong and create a log with both a image of the problem and my solution/problem solving skills utilised in fixing said problem. Furthermore I renamed the assets folder so that existing Tilesets Added needed less work to be imported into the file system.

--- learning/include/xml_parser.hpp
+++ learning/include/xml_parser.hpp
@@ -7,5 +7,19 @@
 #include "include/rapidxml_print.hpp"
 #include "include/rapidxml_utils.hpp"
 class file_to_read{
-    
+    std::string path; 
+    int x = 2;
+    std::vector<std::vector<int>> tileIDs;
+    public:
+       file_to_read(std::string input_path){
+        path = input_path; 
+       }
+        void read_xml_file();
 };
+
+void file_to_read::read_xml_file(){
+    // std::string w = "rararra";
+    rapidxml::file<> xmlFile(path.c_str());
+    rapidxml::xml_document<> doc;
+    doc.parse<0>(xmlFile.data());
+}
---
+++ learning/tilesets/Tileset_v1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tileset_v1" tilewidth="16" tileheight="16" tilecount="8588" columns="113">
+ <image source="../Assets/Tileset_v1.png" width="1808" height="1216"/>
+</tileset>
