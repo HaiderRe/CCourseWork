25483fcf9bd3f3077da6fff7b35c2887930bcae4
HaiderRe <haider.rehawi1@gmail.com>
2023-03-15 23:27:36 +0000

    testing
    testuing

--- learning/include/testing_xml_parser.cpp
+++ learning/include/testing_xml_parser.cpp
@@ -6,6 +6,14 @@ int main(){
     std::string a_path = "testing_functionalilty_of_parser.xml";
     my_xml_parser::file_to_read x;
     x.file_to_read_xml(a_path);
+    x.draw_xml_file();
     system("pause");
+    std::string y;
+    while(1) {
+    std::cin >> y;
+    if(y == "b"){
+        break;
+    }
+    } 
     return 1;
 } 
