8fc1b2e3d94ccee42cd25c9c84aa4f257ab9bbeb
HaiderRe <haider.rehawi1@gmail.com>
2023-07-31 20:00:09 +0100

    (Working) Making skills

--- learning/include/player_objects.hpp
+++ learning/include/player_objects.hpp
@@ -138,6 +138,54 @@ void addAnimation(std::string path, int width, int height, std::vector<std::stri
         }
       }
     };
+    class skill{
+      public: 
+      std::vector<Texture2D> textures;
+      int currentFrame = 0;
+      int frames = 0;
+      int frameSpeed = 6;
+      int amount_of_frames = 4;
+      int fWidth = 64; // Width of the source frame
+      int fHeight = 64; // Height of the source frame
+      int sWidth = 64;
+      int sHeight = 64;
+      float rotation = 0.00f;
+      Vector2 position = {0.00f, 0.00f};
+      Rectangle frameRec = {0.00f, 0.00f, 0.00f, 0.00f};
+      Texture2D texture1;
+      std::string path;
+      Rectangle sourceRec = {0.00f, 0.00f, 0.00f, 0.00f};
+      skill(std::string path){
+        textures.push_back(LoadTexture(("Assets/player/fx/" + path).c_str()));
+        texture1 = textures[0];
+        frameRec.width = fWidth;
+        frameRec.height = fHeight;
+       }
+     ~skill(){
+        for(int i = 0; i < textures.size(); i++){
+          UnloadTexture(textures[i]);
+        }
+      }
+    };
+    class skillManager{
+      public:
+      std::vector<skill> skills;
+      int currentSkill;
+      std::string currentSkillName;
+      void addSkill(std::string path){
+        skills.push_back(skill(path));
+      }
+      void update(){
+        if(IsKeyPressed(KEY_ONE)){
+          currentSkill = 0;
+          currentSkillName = "Skill_Cleave_FX"; // Whatever
+        }
+      }
+      skillManager(){
+        addSkill("Skill_Cleave_FX.png"); // Add other skills 
+      }
+    };
+  
     class player{
         public:
         float xCord;
